79c099f0e898510ba06173ede8fedb2632481289
Francisco Serrano <francisco.serrano372@gmail.com>
2020-05-03 00:19:48 -0300

    adds basic api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/francisco-serrano/api-golang-docker
 
 go 1.14
+
+require github.com/gin-gonic/gin v1.6.2
--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	address = "localhost"
+	port    = "8080"
+)
+
+type Server struct {
+	srv *http.Server
+}
+
+func (s *Server) Run() {
+	if err := s.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("error while starting server: %v", err)
+	}
+}
+
+func (s *Server) Shutdown() {
+	log.Print("gracefully shutting down API...")
+
+	if err := s.srv.Shutdown(context.Background()); err != nil {
+		log.Fatalf("error while shutting down server: %v", err)
+	}
+}
+
+func CreateServer() *Server {
+	router := gin.Default()
+	router.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"hello": "world",
+		})
+	})
+
+	return &Server{srv: &http.Server{
+		Addr:    fmt.Sprintf("%s:%s", address, port),
+		Handler: router,
+	}}
+}
+
+func CreateSignalChannel() chan os.Signal {
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT)
+
+	return done
+}
 
 func main() {
-	fmt.Println("hello world")
+	myServer := CreateServer()
+
+	go myServer.Run()
+
+	done := CreateSignalChannel()
+	<-done
+
+	myServer.Shutdown()
 }
